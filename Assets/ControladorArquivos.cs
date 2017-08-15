16b8c49950d74b0359d5b8267a7072a31273fea7
jsnunes <jesiel.nunes@hotmail.com>
2017-08-15 16:30:37 -0300

    Montando o Projeto #1

---
+++ Assets/ControladorArquivos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ControladorArquivos {
+	private string _caminho;
+
+	public ControladorArquivos(){
+		this._caminho = Application.dataPath;
+	}
+
+	public bool validarDiretorio(string nome){
+		return Directory.Exists (this._caminho+"/"+nome);
+	}
+
+	public bool validarArquivo(string nome){
+		return File.Exists (this._caminho+"/"+nome);
+	}
+
+	public void criarDiretorio(string nome){
+		if (this.validarDiretorio (nome)) {
+			Debug.Log("Esse diretorio ja existe.");
+		} else {
+			Directory.CreateDirectory(this._caminho+"/"+nome);
+		}
+	}
+
+	public void criarArquivo(string nome, string conteudo){
+		if (this.validarArquivo (nome)) {
+			Debug.Log("Esse arquivo ja existe");
+		} else {
+			var arquivo = File.CreateText(this._caminho+"/"+nome);
+			arquivo.WriteLine(conteudo);
+			arquivo.Close();
+		}
+	}
+}
---
+++ Assets/ControladorJogador.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorJogador : MonoBehaviour {
+	private ControladorArquivos _ctlArquivos;
+
+	// Use this for initialization
+	void Start () {
+		_ctlArquivos = new ControladorArquivos ();
+		_ctlArquivos.criarDiretorio ("dados");
+		_ctlArquivos.criarArquivo ("dados/player.json","Teste");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
